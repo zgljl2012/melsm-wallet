c833fa137e069b613cb7e50129b7fa4341f638b9
liaojl <liaojl@cpchain.io>
2021-04-16 14:01:29 +0800

    use react-native-material-ui-x be the UI framework

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-material-ui-x'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button raised primary text="text1" />
+      <Button raised accent text="Accent" />
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
